839297ddcfff415278ee70a7ab7acedddfac7fbf
aiwaka <kw.i.ku.1414@gmail.com>
2023-11-18 22:12:37 +0900

    implement mosaic

--- src/filter/mosaic.rs
+++ src/filter/mosaic.rs
@@ -4,10 +4,10 @@ use crate::process::FilterProcessor;
 
 #[derive(Debug, Clone)]
 pub struct MosaicFilterOption {
-    pub size: u32,
+    pub size: usize,
 }
 impl MosaicFilterOption {
-    pub fn new(size: u32) -> Self {
+    pub fn new(size: usize) -> Self {
         Self { size }
     }
 }
@@ -27,7 +27,19 @@ impl FilterProcessor for MosaicFilter {
         &self,
         buf: &ImageBuffer<image::Rgb<u8>, Vec<u8>>,
     ) -> ImageBuffer<image::Rgb<u8>, Vec<u8>> {
-        buf.to_owned()
-        // TODO:
+        let MosaicFilterOption { size } = self.option;
+        let (buf_width, buf_height) = buf.dimensions();
+        let mut result_buf = buf.clone();
+        for block_y in (0..buf_height).collect::<Vec<u32>>().chunks(size) {
+            for block_x in (0..buf_width).collect::<Vec<u32>>().chunks(size) {
+                let &block_pix = buf.get_pixel(block_x[0], block_y[0]);
+                for &y in block_y {
+                    for &x in block_x {
+                        result_buf.put_pixel(x, y, block_pix);
+                    }
+                }
+            }
+        }
+        result_buf
     }
 }
